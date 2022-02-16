b94b8ee99bb0151b0fe8b08b2d5e54e6e97859f4
203 <203@academicos.cesde.edu.co>
2022-02-16 10:05:58 -0500

    se crea controlador

---
+++ js/controladorTienda.js
@@ -0,0 +1,30 @@
+//Entradas del problema
+
+/*
+
+esto es un comentario 
+   de bloque
+ */
+//Entradas==Variables==Constantes
+let nombreProducto="Termo"
+let precioProducto=400000
+let pesoProducto=18.5
+let tieneDescuento=true
+let descripcionProducto="Termo de Atletico Nacional"
+let telefonoVendedor="3225332041"
+
+const NOMBRE_CLIENTE="Deiby"
+//Arreglos unidimensionales(vectores)
+//sirven para almacenar varios elementos en 1 sola  variable
+let productos=["Termo","Camiseta","Subadera","Gorra"]
+let productos2=Array("Termo","Camiseta","Subadera","Gorra")
+
+
+//Salidas
+console.log("buenas")
+console.log(nombreProducto)
+console.log("Su nombre es: "+NOMBRE_CLIENTE+" Y su producto es: "+nombreProducto)
+console.log(`Su nombre es: ${NOMBRE_CLIENTE} y su producto es ${nombreProducto}`)
+console.log("**********")
+console.log(productos)
+console.log(productos2[0])
