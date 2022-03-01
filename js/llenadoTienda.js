8bcd2c31563fdaa8d844a77c9098532f96ba4b87
Deiby <deiby.sancez1@gmail.com>
2022-03-01 17:41:39 -0500

    Se termino los productos del controlador de llenado

--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -3,7 +3,17 @@
 let productos = [
 
 {nombre:"Termo",precio:20000,descripcion:"Termo de atl nacional"},
-{nombre:"camiseta",precio:150000,descripcion:"Camiseta original atl nacional"}
+{nombre:"camiseta",precio:150000,descripcion:"Camiseta original atl nacional"},
+{nombre:"Gorra",precio:75000,descripcion:"Gorra original atl nacional"},
+{nombre:"Mameluco",precio:40000,descripcion:"Mameluco original atl nacional"},
+{nombre:"Buzo",precio:125000,descripcion:"Buzo original atl nacional"},
+{nombre:"Chaqueta",precio:150000,descripcion:"Chaqueta original atl nacional"},
+{nombre:"Tapabocas",precio:11000,descripcion:"Tapabocas original atl nacional"},
+{nombre:"Llavero",precio:20000,descripcion:"Llavero atl nacional"},
+{nombre:"Boligrafo",precio:6000,descripcion:"Boligrafo atl nacional"},
+{nombre:"Poster",precio:12000,descripcion:"Poster  atl nacional"}
+
+
 
 ]
 
