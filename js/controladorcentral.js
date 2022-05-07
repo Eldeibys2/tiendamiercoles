1e879d2cbcc5635aa8b93771cc8ef89e2d41dd94
Deiby <deiby.sancez1@gmail.com>
2022-05-07 17:17:38 -0500

    Terminacion de la tienda

--- js/controladorcentral.js
+++ js/controladorcentral.js
@@ -28,10 +28,14 @@ filaConte.addEventListener("click",function(evento){
 //rutina para añadir un producto al carrito de compras
 let carrito=[]
 
-let botonAgregarCarrito = document.getElementById("botonadd")
+
 
 let botonclear= document.getElementById("botonclear")
-
+botonclear.addEventListener("click",function(){
+    carrito=[]
+    let capsula=document.getElementById("capsula")
+    capsula.classList.add("invisible")
+})
 
 
 //rutina para ver el carrito
@@ -51,23 +55,63 @@ botonvercarrito.addEventListener("click",function(){
         columna1.classList.add("col-4")
 
         let columna2=document.createElement("div")
-        columna1.classList.add("col-8")
+        columna2.classList.add("col-8")
 
         let foto = document.createElement("img")
         foto.classList.add("w-100","img-fluid")
         foto.src=producto.foto
+        
+        let titulo=document.createElement("h2")
+        titulo.classList.add("text-center")
+        titulo.textContent=producto.nombre
+
+        let cantidad=document.createElement("h4")
+        cantidad.classList.add("text-center")
+        cantidad.textContent="Cantidad: "+producto.cantidad
+
+        let precio=document.createElement("h4")
+        precio.classList.add("text-center")
+        precio.textContent="Precio: $"+producto.precio
+
+
+        let subtotal=document.createElement("h4")
+        subtotal.classList.add("text-center")
+        subtotal.textContent="Subtotal: $"+producto.cantidad*producto.precio
+
 
 
         //padre e hijos
         columna1.appendChild(foto)
+        columna2.appendChild(titulo)
+        columna2.appendChild(cantidad)
+        columna2.appendChild(precio)
+        columna2.appendChild(subtotal)
         fila.appendChild(columna1)
         fila.appendChild(columna2)
         basecarro.appendChild(fila)
 
     })
-
+    let total=0
+    carrito.forEach(function(producto){
+        total=total+Number(producto.cantidad*producto.precio)
+    })
+    
+    
+    let totalCompra = document.getElementById("total")
+    totalCompra.textContent= "Total: $"+total
 
     modalcompra.show()
+
+    let btndolar=document.getElementById("btndolar")
+    btndolar.addEventListener("click",function(){
+        
+    let pesos = total
+    let dolar = pesos/4000
+    totalCompra.textContent="Total en dolares: $"+dolar.toFixed(2)
+    })
+
+
+
 })
 
 
@@ -75,13 +119,9 @@ botonvercarrito.addEventListener("click",function(){
 
 
 
-botonclear.addEventListener("click",function(){
-    carrito=[]
-    let capsula=document.getElementById("capsula")
-    capsula.classList.add("invisible")
-})
 
 
+let botonAgregarCarrito = document.getElementById("botonadd")
 botonAgregarCarrito.addEventListener("click",function () {
     //debo capturar la cantidad y agregarla al producto
     let cantidad = document.getElementById("cantidadproducto").value
@@ -101,7 +141,18 @@ botonAgregarCarrito.addEventListener("click",function () {
     let capsula=document.getElementById("capsula")
     capsula.textContent=suma
     capsula.classList.remove("invisible")
+
     console.log(carrito)
     modalinfo.hide()
+    reiniciar()
     
 })
+
+
+
+
+
+function reiniciar(){
+    let cantidadProducto = document.getElementById("cantidadproducto")
+    cantidadProducto.value="1"
+}
--- js/sumar.js
+++
@@ -1,7 +0,0 @@
-//Entradas
-
-function sumar(numero1,numero2){
-    console.log (" la suma es: "+(numero1 + numero2))
-}
-
-sumar(5,6)
